4c13356934bcc217703df7118fdc41dbc18ed776
adam-pop <adamrasouli8@gmail.com>
2025-02-23 23:24:57 +0800

    Improved and added athe navbar icons. Also deleted the background colour for the device handling.

--- Frontend-DevFolder/Main/Pages/Assets/JS/navbar-footer-loader.js
+++ Frontend-DevFolder/Main/Pages/Assets/JS/navbar-footer-loader.js
@@ -158,9 +158,9 @@ document.addEventListener("DOMContentLoaded", function () {
     
             // Default Navbar Links
             navLinks.innerHTML = `
-                <li><a href="index.html">Home</a></li>
-                <li><a href="About.html">About</a></li>
-                <li><a href="Contact.html">Contact</a></li>
+                <li><a href="index.html"><i class="fa fa-home"></i> Home</a></li>
+                <li><a href="About.html"><i class="fa fa-info-circle"></i> About</a></li>
+                <li><a href="Contact.html"><i class="fa fa-phone"></i> Contact</a></li>
             `;
         }
     
