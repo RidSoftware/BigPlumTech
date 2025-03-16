963f922970486502a16c91b9b3d33e03a9636e8f
Calum Murray <cm2103@hw.ac.uk>
2025-03-16 14:48:55 +0000

    debuggin parser error

--- HomeSystemWebApp/frontEnd/Pages/Assets/JS/deviceAPI.js
+++ HomeSystemWebApp/frontEnd/Pages/Assets/JS/deviceAPI.js
@@ -26,7 +26,7 @@ export const syncDevicesFromBackend = async (userID) => {
 
         const result = await response.json();
         console.log("raw json in front api: ", result);
-        console.log("raw json in front api: ", result.saveDevices);
+        console.log("raw json in front api: ", result.sentDevices);
         if (!result.success) throw new Error(result.message);
 
         saveDevices(JSON.parse(result.sentDevices)); // updates devices obj locallstoraeg
