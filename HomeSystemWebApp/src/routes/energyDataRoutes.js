00e0ce7d3fc654fc98b1d828557b95f2bf6df901
Calum Murray <cm2103@hw.ac.uk>
2025-03-17 18:27:36 +0000

    fixed poor formatting of week data

--- HomeSystemWebApp/src/routes/energyDataRoutes.js
+++ HomeSystemWebApp/src/routes/energyDataRoutes.js
@@ -150,11 +150,8 @@ router.post('/api/pull7days', async (req, res) => {
 
         // Initialize a data structure to ensure all 7 days are represented
         const energyWeekProcessed = {};
-        for (let i = 0; i < 7; i++) {
-            const date = new Date();
-            date.setDate(date.getDate() - i);
-            const formattedDate = date.toISOString().split('T')[0];
-            energyWeekProcessed[formattedDate] = 0;
+        for (let i = 0; i < 24; i++) {
+            energyWeekProcessed[i] = 0; 
         }
 
         // Populate energy data
