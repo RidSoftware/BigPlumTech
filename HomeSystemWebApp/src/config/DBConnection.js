89e3569102681a2d60e7f4ede407fb38fed72b76
Calum Murray <cm2103@hw.ac.uk>
2025-03-19 05:29:08 +0000

    dbconfig

--- HomeSystemWebApp/src/config/DBConnection.js
+++ HomeSystemWebApp/src/config/DBConnection.js
@@ -2,16 +2,10 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: "localhost",
-<<<<<<< HEAD
     user: "SmallPlum",//SmallPlum
     password: "PlumPassword",//PlumPassword
     database: "test2",//test2
-=======
-    user: "root",
-    password: "plumPassword",
-    database: "plumDB",
->>>>>>> 345d859ef39db884656d224da714c3b85bc763a1
-    port: 3306
+
 });
 
 
