89bf121f483ffeee36e465c67492b2dcd41a4459
Calum Murray <cm2103@hw.ac.uk>
2025-03-19 07:13:38 +0000

    contact page and email service and contactapi

--- HomeSystemWebApp/src/server.js
+++ HomeSystemWebApp/src/server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const path = require('path');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const bodyParser = require("body-parser");
 require("dotenv").config(); // Load environment variables
@@ -12,12 +12,14 @@ require("dotenv").config(); // Load environment variables
 app.use(cors()); 
 app.use(express.json()); 
 app.use(express.static(path.join(__dirname, '../frontEnd'))); // gives access to parent file structures
-
+//app.use(bodyParser.json());
 
 // DB Connection
 const db = require('./config/DBConnection');
 const pool = require('./config/DBPool');
 
+
+const emailRoutes = require("./routes/emailRoutes");
 const userRoutes = require('./routes/userRoutes');
 const energyDataRoutesUA = require('./routes/energyDataRoutesUserArray');
 const energyDataRoutesDA = require('./routes/energyDataRoutesDeviceArray');
@@ -25,7 +27,9 @@ const energyDataRoutesUI = require('./routes/energyDataRoutesUserInt');
 const energyDataRoutesDI = require('./routes/energyGridRoute');
 const energyGridRoute = require('./routes/energyDataRoutesDeviceInt');
 const deviceDataRoutes = require('./routes/deviceDataRoutes');
+const contactRoutes = require('./routes/contact'); 
 // Routes
+app.use(contactRoutes);
 app.use(userRoutes);
 app.use(energyDataRoutesUA);
 app.use(energyDataRoutesDA);
@@ -33,6 +37,7 @@ app.use(energyDataRoutesUI);
 app.use(energyDataRoutesDI);
 app.use(deviceDataRoutes);
 app.use(energyGridRoute);
+app.use("api", emailRoutes);
 
 app.get('/', (req, res) => {
     res.send('Server is up and running');
