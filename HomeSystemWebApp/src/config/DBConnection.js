fc1477740c67de7eebfe2e4f628fed56ed67b32f
gp2018hw <gp2018@hw.ac.uk>
2025-03-19 01:29:06 +0000

    Added a live energy update method (hopefully)

--- HomeSystemWebApp/src/config/DBConnection.js
+++ HomeSystemWebApp/src/config/DBConnection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: "localhost",
-    user: "SmallPlum",//SmallPlum
-    password: "PlumPassword",//PlumPassword
-    database: "test2",//test2
+    user: "root",
+    password: "plumPassword",
+    database: "plumDB",
     port: 3306
 });
 
