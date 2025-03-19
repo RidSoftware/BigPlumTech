a90eb55c3ee774b6b35dc714302c3671d002e614
Calum Murray <cm2103@hw.ac.uk>
2025-03-19 05:14:47 +0000

    adding cost to device overview

--- HomeSystemWebApp/frontEnd/Pages/Assets/JS/Overview.js
+++ HomeSystemWebApp/frontEnd/Pages/Assets/JS/Overview.js
@@ -381,7 +381,7 @@ async function loadDeviceStatus() {
             <p>Status: <span class="device-status ${device.status ? "active" : "inactive"}">${device.status ? "On" : "Off"}</span></p>
             <p>Room: ${device.room || "Not assigned"}</p>
             <p>Past 24hr Usage: <strong>${device.usage24hr}</strong> kWh</p>
-            <p>Past 24hr Cost: <strong>${(device.usage24hr)*(localStorage.getItem("carbonIntensity"))}</strong> kWh</p>
+            <p>Past 24hr Cost: <strong>${(device.usage24hr)*(localStorage.getItem("energyCost"))}</strong> kWh</p>
           `;
           
           deviceContainer.appendChild(deviceCard);
